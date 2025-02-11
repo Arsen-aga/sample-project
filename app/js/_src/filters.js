72173dbee8cf4a318926dfa0fff5aa98a85fb1ec
Arsen Asanov <tatarin08082002@gmail.com>
2025-02-11 15:01:11 +0300

    added section-19 and change filters script

--- app/js/_src/filters.js
+++ app/js/_src/filters.js
@@ -1,9 +1,11 @@
 const filters = document.querySelectorAll(".filters");
 
-if (filters) {
+if (filters.length > 0) {
   filters.forEach((filter) => {
     const btns = filter.querySelectorAll(".filters-btn");
     const items = filter.querySelectorAll(".filters-item");
+    const cursors = filter.querySelectorAll(".coursor");
+    checkQuantity(btns, cursors[0], true);
 
     btns.forEach((btn) => {
       btn.addEventListener("click", function (e) {
@@ -14,12 +16,24 @@ if (filters) {
 
         items.forEach((item) => {
           if (item.classList.contains(keyWord)) {
-            item.classList.add("active");
-          }else{
-            item.classList.remove("active");
+            item.classList.remove("hidden");
+          } else {
+            item.classList.add("hidden");
           }
         });
+
+        const activeItems = [...items].filter((item) =>
+          item.classList.contains(keyWord)
+        );
+        checkQuantity(activeItems, cursors[1]);
       });
     });
   });
+}
+
+function checkQuantity(list, cursor, isBtns) {
+  if (window.innerWidth > 991.8) return;
+  if (list.length > 2 && isBtns) cursor.style.display = "flex";
+  else if (list.length > 1 && !isBtns) cursor.style.display = "flex";
+  else cursor.style.display = "none";
 }
